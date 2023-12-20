1cd2b232f4bb92b69ef58a3cd56c923f3c113245
KT220m <tsuchiya-kaito220@g.ecc.u-tokyo.ac.jp>
2023-12-20 12:35:23 +0900

    ~ model3 の量子化部分 . 精度上がらず。バイアスの計算が間違っている可能性

--- model3/quantize_old.py
+++ model3/quantize_old.py
@@ -13,48 +13,9 @@ batch_size = 64
 train_dataloader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
 test_dataloader = torch.utils.data.DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = False)
 
-# BatchNormFolding (model1 -> fmodel)
 weights = torch.load("weight.pth")
 model1 = model.Model()
 model1.load_state_dict(weights)
-fmodel = model.FoldedModel()
-
-w_conv1 = model1.conv1.weight.data
-b_conv1 = model1.conv1.bias.data
-w_bn1 = model1.bn1.weight.data
-b_bn1 = model1.bn1.bias.data
-m_bn1 = model1.bn1.running_mean.data
-v_bn1 = model1.bn1.running_var.data
-
-w_conv1_folded = w_conv1.clone()
-for i in range(w_conv1.shape[0]):
-	w_conv1_folded[i] = w_conv1[i] * w_bn1[i] / v_bn1[i].sqrt()
-b_conv1_folded = (b_conv1 - m_bn1) * w_bn1 / v_bn1.sqrt() + b_bn1
-
-fmodel.conv1.weight.data = w_conv1_folded
-fmodel.conv1.bias.data = b_conv1_folded
-
-w_conv2 = model1.conv2.weight.data
-b_conv2 = model1.conv2.bias.data
-w_bn2 = model1.bn2.weight.data
-b_bn2 = model1.bn2.bias.data
-m_bn2 = model1.bn2.running_mean.data
-v_bn2 = model1.bn2.running_var.data
-
-w_conv2_folded = w_conv2.clone()
-for i in range(w_conv2.shape[0]):
-	w_conv2_folded[i] = w_conv2[i] * w_bn2[i] / v_bn2[i].sqrt()
-b_conv2_folded = (b_conv2 - m_bn2) * w_bn2 / v_bn2.sqrt() + b_bn2
-
-fmodel.conv2.weight.data = w_conv2_folded
-fmodel.conv2.bias.data = b_conv2_folded
-
-fmodel.fc1.weight.data = model1.fc1.weight.data
-fmodel.fc1.bias.data = model1.fc1.bias.data
-fmodel.fc2.weight.data = model1.fc2.weight.data
-fmodel.fc2.bias.data = model1.fc2.bias.data
-
-### fmodel を量子化
 
 # 各relu の活性値の情報
 
@@ -66,18 +27,20 @@ relu3_max = 0
 relu3_min = 0
 
 for (x, t) in train_dataloader:
-	x = fmodel.conv1(x)
+	x = model1.conv1(x)
+	x = model1.bn1(x)
 	x = torch.relu(x)
 	if relu1_max < x.max():
 		relu1_max = x.max()
-	x = fmodel.maxpool1(x)
-	x = fmodel.conv2(x)
+	x = model1.maxpool1(x)
+	x = model1.conv2(x)
+	x = model1.bn2(x)
 	x = torch.relu(x)
 	if relu2_max < x.max():
 		relu2_max = x.max()
-	x = fmodel.maxpool2(x)
+	x = model1.maxpool2(x)
 	x = x.reshape(-1, 32*4*4)
-	x = fmodel.fc1(x)
+	x = model1.fc1(x)
 	x = torch.relu(x)
 	if relu3_max < x.max():
 		relu3_max = x.max()
@@ -87,14 +50,26 @@ s_input = 1 / 255
 z_input = 0
 
 # conv1の重みの量子化パラメータ
-weights = fmodel.conv1.weight.data
-bias = fmodel.conv1.bias.data
+weights = model1.conv1.weight.data
+bias = model1.conv1.bias.data
 z_conv1 = 0
+# BN folding
+w_bn1 = model1.bn1.weight.data
+b_bn1 = model1.bn1.bias.data
+m_bn1 = model1.bn1.running_mean.data
+v_bn1 = model1.bn1.running_var.data
 
-s_conv1 = max(weights.max(),weights.min() * -1) / 127
-quantized_w_conv1 = (weights / s_conv1).round()
-quantized_b_conv1 = (bias / s_conv1 / s_input).round()
+w_folded = weights.clone()
+for i in range(w_folded.shape[0]):
+	w_folded[i] = w_bn1[i] / v_bn1[i].sqrt() * weights[i]
+b_folded = b_bn1 + (bias - m_bn1) * w_bn1 / v_bn1.sqrt()
 
+s_conv1 = max(w_folded.max(), w_folded.min() * -1) / 127
+quantized_w_conv1 = (w_folded / s_conv1).round()
+quantized_b_conv1 = (b_folded / s_conv1 / s_input).round()
+
+print(quantized_w_conv1[0])
+print(quantized_b_conv1)
 # conv1 + relu の活性値の量子化パラメータ
 s_relu1 =  relu1_max / 255
 M = s_relu1 / s_input / s_conv1
@@ -104,13 +79,23 @@ while M >= 2:
 	shiftM1 += 1
 
 # conv2の重みの量子化パラメータ
-weights = fmodel.conv2.weight.data
-bias = fmodel.conv2.bias.data
+weights = model1.conv2.weight.data
+bias = model1.conv2.bias.data
 z_conv2 = 0
+# BN folding
+w_bn2 = model1.bn2.weight.data
+b_bn2 = model1.bn2.bias.data
+m_bn2 = model1.bn2.running_mean.data
+v_bn2 = model1.bn2.running_var.data
 
-s_conv2 = max(weights .max(), weights .min() * -1) / 127
-quantized_w_conv2 = (weights / s_conv2).round()
-quantized_b_conv2 = (bias / s_conv2 / s_input).round()
+w_folded = weights.clone()
+for i in range(w_folded.shape[0]):
+	w_folded[i] = w_bn2[i] / v_bn2[i].sqrt() * weights[i]
+b_folded = b_bn2 + (bias - m_bn2) * w_bn2 / v_bn2.sqrt()
+
+s_conv2 = max(w_folded.max(), w_folded.min() * -1) / 127
+quantized_w_conv2 = (w_folded / s_conv2).round()
+quantized_b_conv2 = (b_folded / s_conv2 / s_input).round()
 
 # conv2 + relu の活性値の量子化パラメータ
 s_relu2 = relu2_max / 255
@@ -121,8 +106,8 @@ while M >= 2:
 	shiftM2 += 1
 
 # fc1 の重みの量子化パラメータ
-weights = fmodel.fc1.weight.data
-bias = fmodel.fc1.bias.data
+weights = model1.fc1.weight.data
+bias = model1.fc1.bias.data
 s_fc1 = max(weights.max(), weights.min() * -1) / 127
 z_fc1 = 0
 quantized_w_fc1 = (weights / s_fc1).round()
@@ -137,8 +122,8 @@ while M >= 2:
 	shiftM3 += 1
 
 # fc2 の重みの量子化パラメータ
-weights = fmodel.fc2.weight.data
-bias = fmodel.fc2.bias.data
+weights = model1.fc2.weight.data
+bias = model1.fc2.bias.data
 s_fc2 = max(weights.max() , weights.min() * -1) / 127
 z_fc2 = 0
 quantized_w_fc2 = (weights / s_fc2).round()
@@ -159,7 +144,6 @@ qmodel.fc2.bias.data = quantized_b_fc2
 correct = 0
 total = 0
 for (x, t) in test_dataloader:
-	qmodel.eval()
 	x = (x * 255).round()
 	output = qmodel(x)
 	_, predicted = torch.max(output, 1)
